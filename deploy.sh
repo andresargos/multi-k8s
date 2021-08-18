5ce6d073dd8fa6b2625de861a7e3a320d7c65b9a
Andres Avila <andresavilawille@gmail.com>
2021-08-18 16:24:45 -0600

    fixed multiple errors

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t gorillavila/multi-client:latest -t gorillavila/multi-client:$SHA -f ./client/Dockerfile./client
-docker build -t gorillavila/multi-server:latest -t gorillavila/multi-server:$SHA -f ./server/Dockerfile./server
-docker build -t gorillavila/multi-worker:latest -t gorillavila/multi-worker:$SHA -f ./worker/Dockerfile./worker
+docker build -t gorillavila/multi-client:latest -t gorillavila/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t gorillavila/multi-server:latest -t gorillavila/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t gorillavila/multi-worker:latest -t gorillavila/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push gorillavila/multi-client:latest
 docker push gorillavila/multi-server:latest
 docker push gorillavila/multi-worker:latest
@@ -11,5 +11,5 @@ docker push gorillavila/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=gorillavila/multi-server:$SHA
-kubectl set image deployments/client-deployment server=gorillavila/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=gorillavila/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=gorillavila/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=gorillavila/multi-worker:$SHA
